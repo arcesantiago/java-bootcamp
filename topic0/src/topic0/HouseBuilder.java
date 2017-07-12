c45710f184236c49f64defd933c85b0680c5fc9f
santiago arce <arcesantiago90@gmail.com>
2017-07-12 01:43:50 -0300

    build pattern

---
+++ topic0/src/topic0/HouseBuilder.java
@@ -0,0 +1,13 @@
+package topic0;
+
+public interface HouseBuilder {
+	//interface protoype methods
+	void buildArea();
+	void buildAddress();
+	void buildStatus();
+	void buildColor();
+	void buildToilet();
+	void buildBedroom();
+	House getHouse();
+
+}
